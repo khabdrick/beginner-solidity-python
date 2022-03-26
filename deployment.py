4987e559ddec423245a7181793889c8b2ce8e0fd
Muhammed Ali <muhamzyali@gmail.com>
2022-03-26 20:37:13 +0100

    code for compiler

---
+++ deployment.py
@@ -0,0 +1,20 @@
+from solcx import compile_standard, install_solc
+with open("ContactList.sol", "r") as file:
+    contact_list_file = file.read()
+
+install_solc("0.8.0")
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": contact_list_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
