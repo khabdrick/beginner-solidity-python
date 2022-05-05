8762529dc74436e7a65ea65e6ad9991a6fbc2c9e
Muhammed Ali <muhamzyali@gmail.com>
2022-05-05 15:46:03 +0100

    final deployment file

--- deployment.py
+++ deployment.py
@@ -1,6 +1,6 @@
 from solcx import compile_standard, install_solc
 import json
-from  web3 import Web3
+from web3 import Web3
 
 
 with open("ContactList.sol", "r") as file:
@@ -15,7 +15,12 @@ compiled_sol = compile_standard(
         "settings": {
             "outputSelection": {
                 "*": {
-                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"] # output needed to interact with and deploy contract 
+                    "*": [
+                        "abi",
+                        "metadata",
+                        "evm.bytecode",
+                        "evm.bytecode.sourceMap",
+                    ]  # output needed to interact with and deploy contract
                 }
             }
         },
@@ -27,16 +32,22 @@ with open("compiler_output.json", "w") as file:
     json.dump(compiled_sol, file)
 
 # get bytecode
-bytecode = compiled_sol["contracts"]["ContactList.sol"]["ContactList"]["evm"]["bytecode"]["object"]
+bytecode = compiled_sol["contracts"]["ContactList.sol"]["ContactList"]["evm"][
+    "bytecode"
+]["object"]
 
 # get abi
-abi = json.loads(compiled_sol["contracts"]["ContactList.sol"]["ContactList"]["metadata"])["output"]["abi"]
+abi = json.loads(
+    compiled_sol["contracts"]["ContactList.sol"]["ContactList"]["metadata"]
+)["output"]["abi"]
 
 # For connecting to ganache
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 chain_id = 1337
 address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
-private_key = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d" # leaving the private key like this is very insecure if you are working on real world project
+private_key = (
+    "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
+)  # leaving the private key like this is very insecure if you are working on real world project
 
 # Create the contract in Python
 ContactList = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -44,12 +55,7 @@ ContactList = w3.eth.contract(abi=abi, bytecode=bytecode)
 nonce = w3.eth.getTransactionCount(address)
 # build transaction
 transaction = ContactList.constructor().buildTransaction(
-    {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": address,
-        "nonce": nonce,
-    }
+    {"chainId": chain_id, "gasPrice": w3.eth.gas_price, "from": address, "nonce": nonce}
 )
 # Sign the transaction
 sign_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
@@ -60,3 +66,19 @@ transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
 print("Waiting for transaction to finish...")
 transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
 print(f"Done! Contract deployed to {transaction_receipt.contractAddress}")
+
+contact_list = w3.eth.contract(address=transaction_receipt.contractAddress, abi=abi)
+store_contact = contact_list.functions.addContact(
+    "name", "+2348112398610"
+).buildTransaction({"chainId": chain_id, "from": address, "gasPrice": w3.eth.gas_price, "nonce": nonce + 1})
+
+# Sign the transaction
+sign_store_contact = w3.eth.account.sign_transaction(
+    store_contact, private_key=private_key
+)
+# Send the transaction
+send_store_contact = w3.eth.send_raw_transaction(sign_store_contact.rawTransaction)
+
+transaction_receipt = w3.eth.wait_for_transaction_receipt(send_store_contact)
+
+print(contact_list.functions.retrieve().call())
