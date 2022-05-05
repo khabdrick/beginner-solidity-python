9489ab9e8e3e4162f5432a49f515e3304a905e3b
Muhammed Ali <muhamzyali@gmail.com>
2022-05-05 14:48:09 +0100

    complete solidity code

--- ContactList.sol
+++ ContactList.sol
@@ -5,6 +5,8 @@ pragma solidity 0.8.0;
 contract ContactList {
 
     // uint phoneNumber;
+    uint256 phoneNumber;
+
 
     struct Contact {
         // assosiate name with phone number
@@ -16,8 +18,14 @@ contract ContactList {
     Contact[] public contact; //array for list of contacts
     mapping(string => string) public nameToPhoneNumber; //used to map name to phone number, so you can get phone number using name
 
+     
+    function retrieve() public view returns (Contact[] memory){
+        return contact;
+    }
+    
     function addContact(string memory _name, string memory _phoneNumber) public {
         contact.push(Contact(_name, _phoneNumber)); //append to  Contact[] array
         nameToPhoneNumber[_name] = _phoneNumber; //use name to get phone number
     }
+    
 }
